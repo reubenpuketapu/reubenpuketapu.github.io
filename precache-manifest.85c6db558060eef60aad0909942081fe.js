96aae5a95232227bff867676d9c90ae44943ff4b
Reuben Puketapu <reuben.puketapu@xero.com>
2019-01-11 13:56:00 +1300

    Updates

---
+++ precache-manifest.85c6db558060eef60aad0909942081fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a666e60d984cc39f3af1",
+    "url": "/static/css/main.c67188a3.chunk.css"
+  },
+  {
+    "revision": "a666e60d984cc39f3af1",
+    "url": "/static/js/main.a666e60d.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "714e1ac85a387cba420adfa27b507a1b",
+    "url": "/index.html"
+  }
+];
